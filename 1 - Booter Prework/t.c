5ba132faba360bf27b62f85a43ae4d3e4095d691
Sophie Schuur <sophia.schuur@wsu.edu>
2019-01-15 13:05:02 -0800

    Added 1 prework

---
+++ 1 - Booter Prework/t.c
@@ -0,0 +1,115 @@
+
+typedef unsigned char  u8;
+typedef unsigned short u16;
+typedef unsigned long  u32;
+
+#include "ext2.h"
+typedef struct ext2_group_desc  GD;
+typedef struct ext2_inode       INODE;
+typedef struct ext2_dir_entry_2 DIR;
+
+#define BLK 1024
+char buf1[BLK], buf2[BLK];
+int iblock;
+
+int myprints(char *s)
+{
+  while(*s)
+  {
+    putc(*s++);
+  }
+  return 0;
+}
+
+int mygets(char s[]) 
+{ 
+  while ( (*s = getc()) != '\r')
+  {   
+    putc(*s++);
+  }
+  *s = 0;
+  return 0;
+}
+
+u16 getblk(u16 blk, char *buf)
+{
+	//readfd( blk/18, (blk / 9) % 2, (blk*2)%18, buf);
+	readfd( blk/18, ((blk)%18)/9, ( ((blk)%18)%9)<<1, buf);
+}
+
+u16 find(u16 block, char *name)
+{
+	DIR *dirp;
+	char *c;
+	getblk(block, buf2);
+	dirp = (DIR *)buf2;
+	while((char *)dirp < &buf2[BLK]){
+		c = dirp->name[dirp->name_len];
+		dirp->name[dirp->name_len] = 0;
+		if (strcmp(dirp->name, name) == 0){
+			dirp->name[dirp->name_len] = c;
+			return dirp->inode;
+		}
+		dirp->name[dirp->name_len] = c;
+		dirp = (DIR *)((char *)dirp + dirp->rec_len);
+	}
+	return 0;
+}
+
+INODE *gino(u16 blk, u16 node)
+{
+	u16 bk, id;
+	bk = blk + ((node -1) / 8);
+	id = (node - 1) & 7;
+	getblk(bk, buf1);
+
+	return (INODE *)buf1 + id;
+}
+
+char *dev = "mtximage";
+char temp[256];
+main()
+{
+
+	char name[64];
+	u16    i,iblk;
+	char   c;
+	GD    *gp;
+	INODE *ip;
+	DIR   *dp;
+	u32   *bp;
+	char *cp;
+	char *x;
+
+	getblk(2, buf1);
+	
+	myprints("What's your name? ");
+	mygets(name);    
+    myprints("\n\r"); myprints("We've been expecting you, "); myprints(name); myprints("\n\r");
+  
+	gp = (GD *)buf1;
+	iblk = (u16)gp->bg_inode_table;
+
+	getblk((u16)iblk, buf1);  
+	ip = (INODE *)buf1 + 1;   
+
+
+
+  	myprints("inode_block="); putc(iblk+'0'); myprints("\n\r");
+  
+  	getblk((u16)ip->i_block[0], buf2);
+  	dp = (DIR *)buf2;         // buf2 contains DIR entries
+
+
+  	myprints(" Contents of"); myprints(dev); myprints("\n\r");
+
+  	while((char *)dp < &buf2[BLK])
+  	{
+  		x = dp->name[dp->name_len];
+  		dp->name[dp->name_len] = 0;
+  		myprints("\n\r"); myprints(dp->name); 
+  	
+  		dp = (DIR *)((char *)dp + dp->rec_len);
+  	}
+	return 1;
+}
