eb014cf4c5b80dff83364e7505e44b66ba6cc2d0
Sophie Schuur <sophia.schuur@wsu.edu>
2019-04-27 05:43:51 -0700

    Added creat for use as test file to see how to quickly add new commands to wanix

---
+++ 6 - MTX OS/creat.c
@@ -0,0 +1,8 @@
+#include "ucode.c"
+
+//Quick test for adding new custom command to wanix that does not
+//already exist. Add with sh mk creat
+
+int main(int argc, char *argv[]) {
+	creat(argv[1])
+}
